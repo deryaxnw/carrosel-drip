37f4a1d96a74040a69e74b9fc55308ff214d3fca
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-11-05 14:22:38 -0300

    logica de array dos checkbox ihulll

--- src/Components/aside/FilterAside.jsx
+++ src/Components/aside/FilterAside.jsx
@@ -17,6 +17,19 @@ function FilterAside() {
     {name: 'corrida', label: 'Corrida'}
   ];
 
+
+  const genero = [
+    {name: 'masculino', label:'Masculino'},
+    {name: 'feminino', label:'Feminino'},
+    {name: 'unisex', label: 'Unisex'}
+  ]
+
+  const estado = [
+    {name: 'novo', label: 'Novo'},
+    {name: 'Usado', label: 'Usado'}
+  ]
+
+
   const [selectedMarkas, setSelectedMarkas] = useState(
     marka.reduce((acc) => {
       acc[marka.name] = false;
@@ -31,14 +44,40 @@ function FilterAside() {
     }, {})
   );
 
-  // Função para manipular o clique em cada checkbox
-  const handleCheckboxChange = (event) => {
+
+const [selectedGenero, setSelectedGenero] = useState(
+  genero.reduce((acc) => {
+    acc[genero.name] = false;
+    return acc;
+  }, {})
+)
+
+const [selectedEstado, setSelectedEstado] = useState(
+  estado.reduce((acc) => {
+    acc[estado.name] = false;
+    return acc;
+  }, {})
+)
+
+  const handleCheckboxChange = (event, setFunction) => {
     const { name, checked } = event.target;
-    setSelectedBrands((prevState) => ({
+    setFunction((prevState) => ({
       ...prevState,
       [name]: checked,
     }));
   };
+
+  const handleRadioChange = (event) => {
+    const { name } = event.target;
+    setSelectedEstado({
+      novo: false,
+      usado: false,
+      [name]: true,
+    });
+  };
+
+
+  
 
   return (
     <aside style={{ padding: '20px', border: '1px solid #ccc', width: '200px' }}>
@@ -52,31 +91,68 @@ function FilterAside() {
               <input
                 type="checkbox"
                 name={marka.name}
-                checked={selectedMarkas[marka.name]}
-                onChange={handleCheckboxChange}
+                checked={!!selectedMarkas[marka.name]}
+                onChange={(e) => {handleCheckboxChange(e, setSelectedMarkas)}}
               />
               {marka.label}
             </label>
           </div>
         ))}
       </div>
-      {/* Nova div adicionada dentro do aside */}
+
       <div style={{ marginTop: '20px', color: 'black' }}>
+      <h3>Categoria</h3>
       {categoria.map((categ) => (
           <div key={categ.name}>
             <label>
               <input
                 type="checkbox"
                 name={categ.name}
-                checked={selectedCategoria[categ.name]}
-                onChange={handleCheckboxChange}
+                checked={!!selectedCategoria[categ.name]}
+                onChange={(e) => {handleCheckboxChange(e, setSelectedCategoria)}}
               />
               {categ.label}
             </label>
           </div>
         ))}
       </div>
+
+      <div style={{ marginTop: '20px', color: 'black' }}>
+      <h3>Gênero</h3>
+      {genero.map((gene) => (
+          <div key={gene.name}>
+            <label>
+              <input
+                type="checkbox"
+                name={gene.name}
+                checked={!!selectedGenero[gene.name]}
+                onChange={(e) => {handleCheckboxChange(e, setSelectedGenero)}}
+              />
+              {gene.label}
+            </label>
+          </div>
+        ))}
+      </div>
+
+      <div style={{ marginTop: '20px', color: 'black' }}>
+      <h3>Estado</h3>
+      {estado.map((estad) => (
+          <div key={estad.name}>
+            <label>
+              <input
+                type="radio"
+                name="estado"
+                value={estad.name}
+                checked={selectedEstado[estad.name] || true}
+                onChange={handleRadioChange}
+              />
+              {estad.label}
+            </label>
+          </div>
+        ))}
+      </div>
     </aside>
+    
   );
 }
 
